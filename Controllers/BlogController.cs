20b02ad0f615d9273fb6369ac7804047d5bb2417
Tahir9176 <tahirhsnli5@gmail.com>
2023-04-28 06:21:48 +0400

    FronttoBack

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FrontToBack.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult BlogDetails()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FrontToBack.Controllers
+{
+	public class ProductController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
